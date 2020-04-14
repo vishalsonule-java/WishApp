e0f1d1b5df7aa5b3ce7b0b17cf179dbeff19ed3b
mangesh <vishalsonuler@gmail.com>
2020-04-14 14:01:39 +0530

    clien app edit by dev2

--- WishApplication/src/com/nt/test/ClientApp.java
+++ WishApplication/src/com/nt/test/ClientApp.java
@@ -1,10 +1,14 @@
 package com.nt.test;
 
+import com.nt.wish.WishMessageGenerator;
+
 public class ClientApp {
 
 	public static void main(String[] args) {
 		
-
+		WishMessageGenerator wmsg=new WishMessageGenerator();
+		String str=wmsg.wish();
+		System.out.println(str);
 	}
 
 }
